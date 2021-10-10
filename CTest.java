55c4d6c5be54561443f554b4abe48ead03fffb7e
ul6x06 <91857915+ul6x06@users.noreply.github.com>
2021-10-11 00:04:28 +0800

    Add files via upload

---
+++ CTest.java
@@ -0,0 +1,14 @@
+package Java08;
+
+public class CTest {
+	void test(int a) {
+		System.out.print(a);
+		if (a % 2 == 1)
+			System.out.println(",计计");
+		else if (a == 0)
+			System.out.println(",计 0");
+		else
+			System.out.println(",计案计");
+
+	}
+}
---
+++ Class07.java
@@ -0,0 +1,13 @@
+package Java08;
+
+public class Class07 {
+
+	public static void main(String[] args) {
+CTest i = new CTest(); 
+i.test(3);
+i.test(8);
+i.test(0);
+
+	}
+
+}
